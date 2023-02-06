ac80dcd9d97cfa961864dfa0b6af15affc9d7dc6
kennedy <kenneyrit@gmail.com>
2023-02-06 14:50:55 +0300

    done

--- app.rb
+++ app.rb
@@ -21,3 +21,10 @@ def add num1, num2
 end
 
 puts add(1,3) + add(2,2)
+
+# app.rb
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+# app.rb
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]

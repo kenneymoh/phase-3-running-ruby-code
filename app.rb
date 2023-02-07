7acde63eb5e4095260ccef282b3c45b5a8ce3b33
kennedy <kenneyrit@gmail.com>
2023-02-07 04:51:10 +0300

    done

--- app.rb
+++ app.rb
@@ -1,30 +1,3 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
-
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
-
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
-
-# app.rb
-print "Hello world!"
-print "Hello world!"
-print "Hello world!"
-# app.rb
-pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+it 'outputs the string "Pass this test, please." using the print method' do
+    expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
+  end

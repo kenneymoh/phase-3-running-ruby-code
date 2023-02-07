0d999b963746e592c147ab6b10c3188cb292ebf8
kennedy <kenneyrit@gmail.com>
2023-02-07 08:38:58 +0300

    done

--- app.rb
+++ app.rb
@@ -1,3 +1,9 @@
-it 'outputs the string "Pass this test, please." using the print method' do
-    expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
-  end
+# it 'outputs the string "Pass this test, please." using the print method' do
+#     expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
+#  end
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
